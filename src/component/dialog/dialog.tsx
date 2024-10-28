469cdf23818d4cb3c7f408700ea1977d9bb0e4bf
Víctor Reyes <unhill@gmail.com>
2024-10-28 11:29:04 +0100

    Show and close Dialog depending on boolean prop showDialog

--- src/component/dialog/dialog.tsx
+++ src/component/dialog/dialog.tsx
@@ -1,13 +1,19 @@
-import { ReactNode, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 type Props = {
   title: string;
+  showDialog: boolean;
   children?: [ReactNode] | ReactNode;
 };
-export function Dialog({ title, children: content }: Props) {
+export function Dialog({ title, showDialog, children: content }: Props) {
   const dialog = useRef<HTMLDialogElement | null>(null);
 
-  dialog.current?.showModal();
+  useEffect(() => {
+    showDialog ? show() : close();
+  }, [showDialog]);
+
+  const show = () => dialog.current?.show();
+  const close = () => dialog.current?.close();
 
   return (
     <dialog ref={dialog} className="w-96 p-8 rounded-lg">
