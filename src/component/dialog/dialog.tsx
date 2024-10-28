4b90d97c717a907f55ede323d0bcb09e71f0868a
Víctor Reyes <unhill@gmail.com>
2024-10-28 13:16:19 +0100

    Add some padding and gaps between Dialogs elements

--- src/component/dialog/dialog.tsx
+++ src/component/dialog/dialog.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useEffect, useRef } from "react";
+import { Button } from "./button";
 
 type Props = {
   title: string;
@@ -7,6 +8,7 @@ type Props = {
   onCancel?: () => void;
   children?: [ReactNode] | ReactNode;
 };
+
 export function Dialog({
   title,
   showDialog,
@@ -24,14 +26,18 @@ export function Dialog({
   const close = () => dialog.current?.close();
 
   return (
-    <dialog ref={dialog} className="w-96 p-8 rounded-lg">
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <main>{content}</main>
-      <div className="flex gap-4 justify-between">
-        <button onClick={onOk}>Ok</button>
-        <button onClick={onCancel}>Cancel</button>
+    <dialog ref={dialog} className="w-96 px-8 py-4 rounded-lg">
+      <div className="flex flex-col gap-4 justify-between">
+        <header className="text-center">
+          <h1>{title}</h1>
+        </header>
+        <hr />
+        <main>{content}</main>
+        <hr />
+        <div className="flex gap-4 justify-between">
+          <Button onClick={onOk}>Ok</Button>
+          <Button onClick={onCancel}>Cancel</Button>
+        </div>
       </div>
     </dialog>
   );
