01581d9794a1fe47a72aa76c1c1e35bc1bbbda16
Víctor Reyes <unhill@gmail.com>
2024-10-28 11:06:29 +0100

    Make dialog accept Title as string and optional content children

--- src/component/dialog/dialog.tsx
+++ src/component/dialog/dialog.tsx
@@ -1,6 +1,10 @@
-import { useRef } from "react";
+import { ReactNode, useRef } from "react";
 
-export function Dialog() {
+type Props = {
+  title: string;
+  children?: [ReactNode] | ReactNode;
+};
+export function Dialog({ title, children: content }: Props) {
   const dialog = useRef<HTMLDialogElement | null>(null);
 
   dialog.current?.showModal();
@@ -8,14 +12,9 @@ export function Dialog() {
   return (
     <dialog ref={dialog} className="w-96 p-8 rounded-lg">
       <header>
-        <h1>Dialog title</h1>
+        <h1>{title}</h1>
       </header>
-      <main>
-        Dialog content: Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Sed eos inventore sint tenetur a vitae dolorem ipsum delectus
-        magni voluptas. Mollitia, unde itaque in deserunt quaerat quam
-        repellendus eius minus.
-      </main>
+      <main>{content}</main>
     </dialog>
   );
 }
