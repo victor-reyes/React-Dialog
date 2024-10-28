9e398fd6033d5226bb4f3daa8e49a77541a5690e
Víctor Reyes <unhill@gmail.com>
2024-10-28 11:07:17 +0100

    Add Lorem content to App for testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,120 @@
-import "./App.css";
+import { Dialog } from "./component/dialog/dialog";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen min-w-screen bg-slate-200 ">
+      <article className="flex flex-col gap-8 p-32">
+        <section className="flex gap-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit repellendus praesentium quia fuga excepturi earum
+            cupiditate doloribus sequi fugiat sapiente mollitia rerum, minima in
+            assumenda? Quis odio nostrum, cum quasi deleniti corrupti facilis et
+            facere aperiam saepe molestias quia similique ipsam illum ex
+            deserunt. Repellat dolorem nam saepe? Maiores repellat officiis
+            accusamus. Vitae aliquam labore consequatur debitis adipisci, hic
+            magni, delectus iusto dolorem corrupti quas itaque eveniet culpa
+            repellat nostrum excepturi est recusandae fugit. Aspernatur nobis
+            blanditiis voluptatem consequuntur facere hic facilis, molestias
+            doloribus quos magni aut quia voluptates minima quae, nemo inventore
+            perferendis? Qui sed vero ex fuga aperiam accusamus provident? Esse,
+            quos at. Error nulla ea asperiores, fuga consectetur nostrum
+            praesentium consequatur ratione libero minus, molestias eligendi
+            perspiciatis neque magni tempore beatae eos?
+          </p>
+          <p>
+            Quaerat eligendi debitis libero voluptas placeat quam possimus
+            soluta atque temporibus blanditiis dolor, voluptatum odit quas
+            veritatis quibusdam beatae ea accusamus suscipit quis laborum
+            facere. Necessitatibus iste tempore provident rem atque officiis
+            magni nisi, aliquam dolore amet reiciendis earum laboriosam
+            quibusdam commodi! Iste modi tempora accusamus rerum, ullam ad fugit
+            voluptatem, deserunt commodi quisquam est obcaecati repudiandae
+            omnis vitae aut ipsam possimus! Aspernatur, atque fugiat!
+            Repudiandae explicabo obcaecati error quisquam exercitationem at
+            labore nulla minima hic, necessitatibus culpa voluptates adipisci
+            illo eligendi molestias. Amet officia est, error id autem expedita
+            porro pariatur molestiae doloremque non incidunt veritatis magnam
+            illo quas distinctio temporibus quo saepe rem optio laboriosam esse
+            exercitationem doloribus et. Quidem vel ea dolor ipsam minus ipsa
+            expedita, repellendus est, enim blanditiis, quaerat eligendi?
+          </p>
+        </section>
+        <section className="flex gap-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            laborum vel ipsum ipsam officia ut. Architecto omnis cumque ut
+            dignissimos ex maxime corrupti non in ducimus accusamus illum, quas
+            quos suscipit earum adipisci. Minus sunt architecto cum sapiente
+            accusantium quae ab laborum corporis velit voluptatem nulla hic
+            corrupti amet qui, quisquam, nesciunt ut facilis. Pariatur,
+            molestias. Suscipit exercitationem dolor eos ea debitis itaque
+            praesentium inventore quas iure libero iusto labore assumenda neque
+            enim error nobis atque, consectetur laboriosam ad aperiam et! Earum
+            accusamus ipsam dolorem nobis mollitia rerum explicabo? Dicta quam
+            at saepe, culpa omnis corporis deserunt fugiat laudantium ipsa
+            numquam perspiciatis dolorem. Quidem recusandae quasi nostrum esse,
+            ipsa quae nam deserunt! Neque molestiae vel dignissimos, earum
+            nostrum odio asperiores adipisci, ab praesentium deserunt aperiam.
+          </p>
+          <p>
+            Possimus suscipit, magni quas pariatur doloribus praesentium
+            expedita quaerat eum autem cumque ex alias vero explicabo voluptatem
+            modi recusandae debitis ab temporibus! Expedita dolorum dolore
+            dignissimos sed maxime sint non delectus tenetur reiciendis dolorem
+            dolores quibusdam hic natus odit voluptates quas, eum aliquid
+            officiis inventore officia enim perferendis debitis sunt. Asperiores
+            numquam illum rerum voluptatum obcaecati, cupiditate expedita ex
+            nisi vitae possimus quod recusandae voluptas harum explicabo ab
+            libero. Ut iste cumque voluptates, quis laborum ipsa id minima
+            beatae dolore consectetur exercitationem eum consequuntur!
+            Repellendus assumenda voluptatem deserunt laborum, quos earum ipsa
+            quidem repudiandae possimus architecto veniam et aspernatur
+            consequuntur at voluptate accusamus saepe temporibus impedit
+            corporis, non ea. Adipisci voluptate placeat eos eum, exercitationem
+            nulla corrupti, quibusdam assumenda nobis voluptatum aliquam minus
+            neque magni!
+          </p>
+        </section>
+        <section className="flex gap-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iusto
+            impedit vitae iste delectus. Ea dolorum cumque, necessitatibus culpa
+            quibusdam, aut soluta similique dolorem corporis saepe, dolor ullam
+            minima modi impedit dolore temporibus obcaecati laudantium ex unde
+            ad recusandae placeat neque voluptatem. Fugit, impedit odio. Eveniet
+            amet itaque alias doloremque maxime, corrupti velit animi quis
+            accusantium atque nulla eum sequi, aut, totam consequuntur. Ratione,
+            natus. Maxime aliquid amet recusandae, quas exercitationem esse
+            incidunt cum tempora id nemo nostrum iure fuga, cupiditate fugiat ea
+            dolore voluptatum aspernatur doloribus reprehenderit rem. Deserunt
+            accusamus totam itaque eveniet! Laboriosam, cupiditate molestias non
+            vero similique dolorum at architecto delectus blanditiis, omnis eius
+            natus exercitationem deleniti fugit, corporis quae? Voluptas, velit
+            nesciunt unde tempore accusamus sit tenetur? Cupiditate deserunt ut
+            corrupti.
+          </p>
+          <p>
+            Quae quis corporis, numquam molestiae fuga at incidunt repellendus
+            voluptas eos eaque repudiandae ad cupiditate iste labore dolor?
+            Soluta id, iusto deleniti excepturi, doloribus laudantium maxime
+            ipsa recusandae nulla est minima eaque vitae blanditiis
+            exercitationem officia cum necessitatibus. Quasi ipsam dolor quidem
+            incidunt officia ea ut doloremque, facere voluptates quibusdam
+            consectetur aut non itaque dignissimos facilis. Dolorem nulla vero
+            aspernatur quaerat quasi facilis quae, ipsum accusantium amet labore
+            nam possimus eligendi, provident veritatis ipsam nobis dolore
+            repellendus error tempora blanditiis non veniam? Praesentium
+            consectetur voluptatem soluta blanditiis sequi ducimus? Neque
+            dolorem deserunt excepturi libero alias eos ducimus repellendus
+            ipsam voluptate a! Ullam voluptates beatae, quisquam deleniti dicta
+            nihil excepturi quas voluptatem maiores id quam accusantium non
+            tempora animi nulla itaque officiis inventore natus pariatur ipsum.
+          </p>
+        </section>
+      </article>
+    </div>
+  );
 }
 
 export default App;
